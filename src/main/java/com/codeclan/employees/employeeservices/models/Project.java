8e93e7dd01ce2a6ba79d0ed13c02520eb49b4b73
BrockBaron <Nslekkas@gmail.com>
2021-06-01 11:43:20 +0100

    Project setup start

--- src/main/java/com/codeclan/employees/employeeservices/models/Project.java
+++ src/main/java/com/codeclan/employees/employeeservices/models/Project.java
@@ -1,22 +1,43 @@
 package com.codeclan.employees.employeeservices.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
 
-import javax.persistence.OneToMany;
-import java.util.ArrayList;
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "projects")
 public class Project {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "duration")
+    private int duration;
 
 
-
+    @ManyToMany
     @JsonIgnoreProperties({"project"})
-    @OneToMany(mappedBy = "project")
-    private List<Project> projects;
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @JoinTable(
+            name = "employee_projects",
+            joinColumns = { @JoinColumn(
+                    name = "project_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "employee_id",
+                    nullable = false,
+                    updatable = false)
+            })
+    private List<Employee> employee;
 
-    public Project(String name, int duration, int noOfEmployees) {
-        this.name = name;
-        this.pirates = new ArrayList<>();
-    }
+
+
 }
