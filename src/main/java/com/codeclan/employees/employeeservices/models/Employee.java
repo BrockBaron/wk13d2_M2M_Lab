b329ad81f9b1962caceea300120b0e7c5ea961ba
BrockBaron <Nslekkas@gmail.com>
2021-06-01 11:39:15 +0100

    Employee setup

--- src/main/java/com/codeclan/employees/employeeservices/models/Employee.java
+++ src/main/java/com/codeclan/employees/employeeservices/models/Employee.java
@@ -1,4 +1,81 @@
 package com.codeclan.employees.employeeservices.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "employeeNumber")
+    private int employeeNumber;
+
+    @JsonIgnoreProperties({"project"})
+    @OneToMany(mappedBy = "project")
+    private List<Project> projects;
+
+    public Employee(Long id, String firstName, String lastName, int employeeNumber, List<Project> projects) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeNumber = employeeNumber;
+        this.projects = new ArrayList<>();
+    }
+
+
+
+//    G&S
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
 }
--- src/main/java/com/codeclan/employees/employeeservices/models/Project.java
+++ src/main/java/com/codeclan/employees/employeeservices/models/Project.java
@@ -1,4 +1,22 @@
 package com.codeclan.employees.employeeservices.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Project {
+
+
+
+
+    @JsonIgnoreProperties({"project"})
+    @OneToMany(mappedBy = "project")
+    private List<Project> projects;
+
+    public Project(String name, int duration, int noOfEmployees) {
+        this.name = name;
+        this.pirates = new ArrayList<>();
+    }
 }
